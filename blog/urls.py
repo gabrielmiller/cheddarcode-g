074fe3693ff12ab728fd43a315f33d74f243e36a
L Gabriel Miller <gmiller2007@gmail.com>
2013-06-27 12:28:11 -0400

    Removes comments.

--- blog/urls.py
+++ blog/urls.py
@@ -5,23 +5,21 @@ from django.contrib.syndication.views import Feed
 
 class BlogFeed(Feed):
     title = "L. Gabriel Miller"
-    description = "Some ramblings of mine"
+    description = "Ramblings of Gabe Miller"
     link = "/blog/feed/"
 
     def items(self):
-        return Post.objects.all().order_by("-created")[:2]
+        return Post.objects.all().order_by("-created")
     def item_title(self, item):
         return item.title
     def item_description(self, item):
         return item.body
     def item_link(self, item):
-        return u"/blog/%d" % item.id
+        return u"/blog/%d" % item.id #update url
 
 urlpatterns = patterns('blog.views',
     url(r'^$', 'bloglist'),
     url(r'^feed/$', BlogFeed()),
-    url(r'^ajax/$', 'ajax'),
-    url(r'^ajaxtag/(?P<tag>\w+)$', 'ajaxtag'),
     url(r'^pk/(?P<pk>\d+)$', 'pkview'),
     url(r'^post/(?P<slug>[\w-]+)$','postview'),
     url(r'^tag/(?P<tag>\w+)$', 'tagpage'),
