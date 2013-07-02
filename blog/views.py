2d59d7f6d728c9ed95cc8672d493b4fdca67746d
L Gabriel Miller <gmiller2007@gmail.com>
2013-07-01 21:06:17 -0400

    Retools CSS. Removes duplicate ids in favor of classes. Adds dynamic date to footer, passed in from each view. Adds sticky footer.

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,29 @@
 from blog.models import Post
 from django.shortcuts import render_to_response
 from django.core.exceptions import ObjectDoesNotExist
+from datetime import datetime
+
+today = datetime.today().year
 
 def article_view(request, year=None, slug=None):
     if year == None:
         post = Post.objects.all().order_by('-created')[0]
-        return render_to_response("post.html", dict(post=post, page="blog"))
+        return render_to_response("post.html", dict(post=post, page="blog", year=today))
     else:
         try:
             min_date = "%s-01-01" % year
             max_date = "%s-12-31" % year
             post = Post.objects.all().filter(created__lte=max_date, created__gte=min_date).get(slug=slug)
-            return render_to_response("post.html", dict(post=post, page="archive"))
+            return render_to_response("post.html", dict(post=post, page="archive", year=today))
         except:
-            return render_to_response("404.html", dict(resource="blog post"))
+            return render_to_response("404.html", dict(resource="blog post", year=today))
 
 def archive_view(request):
     tags = Post.tags.all()
     posts = Post.objects.all().order_by('-created')
-    return render_to_response("archive.html", dict(tags=tags, posts=posts, page="archive"))
+    return render_to_response("archive.html", dict(tags=tags, posts=posts, page="archive", year=today))
 
 def tag_view(request, tag):
     posts = Post.objects.all().order_by('-created').filter(tags__name=tag)
     tags = Post.tags.all()
-    return render_to_response("tag.html", dict(tag=tag, tags=tags, posts=posts, page="archive"))
+    return render_to_response("tag.html", dict(tag=tag, tags=tags, posts=posts, page="archive", year=today))
--- views.py
+++ views.py
@@ -1,12 +1,16 @@
+import datetime
+from datetime import datetime
 from django.shortcuts import render_to_response
 from blog.models import Post
 
+today = datetime.today().year
+
 def about(request):
     page = "about"
-    return render_to_response("about.html", dict(page="about"))
+    return render_to_response("about.html", dict(page="about", year=today))
 
 def projects(request):
-    return render_to_response("projects.html", dict(page="projects"))
+    return render_to_response("projects.html", dict(page="projects", year=today))
 
 def error404(request):
-    return render_to_response("404.html", dict(resource="page"))
+    return render_to_response("404.html", dict(resource="page", year=today))
